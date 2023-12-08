51b8461fc4e6fe037b2f1f51a1e4d88c6c6b8853
leostelon <53221136+leostelon@users.noreply.github.com>
2023-12-08 22:26:18 +0530

    mui and platform select screen

--- src/screens/PlatformSelect.jsx
+++ src/screens/PlatformSelect.jsx
@@ -1,6 +1,42 @@
+import "../styles/PlatformSelect.css";
 import React from "react";
 import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const PlatformSelect = () => {
-	return <Box>PlatformSelect</Box>;
+	const navigate = useNavigate();
+	return (
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				width: "100%",
+				height: "100vh",
+			}}
+		>
+			<Box sx={{ textAlign: "center" }}>Platypus🦫</Box>
+			<Box
+				sx={{
+					flex: 1,
+					alignItems: "center",
+					justifyContent: "center",
+					height: "100%",
+					display: "flex",
+				}}
+			>
+				<Box
+					onClick={() => navigate("/merchant")}
+					className={"platformselect-type"}
+				>
+					Merchant
+				</Box>
+				<Box
+					onClick={() => navigate("/user")}
+					className={"platformselect-type"}
+				>
+					User
+				</Box>
+			</Box>
+		</Box>
+	);
 };
